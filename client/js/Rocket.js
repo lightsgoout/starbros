b2d2bbad06aa83cb456767785f29d85bc5b320a6
UsagiShiroi <dimkrol@mail.ru>
2013-06-09 09:00:15 +0400

    updateRockets

--- client/js/Rocket.js
+++ client/js/Rocket.js
@@ -3,7 +3,7 @@ define(['Point'], function(Point) {
     var Rocket = Class.extend({
         init: function(speed, player_id, power, target_id, life_time) {
             this.pos    = new Point(0, 0);
-            this.radius = 1;
+            this.radius = 10;
             this.life_time = life_time;
             this.speed  = speed;
             this.sprite = sprite;
